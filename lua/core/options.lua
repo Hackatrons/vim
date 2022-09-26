515af6c80bcc834fd9f33fc4fb27c58f5a010e31
Daniel Hack <hack.daniel.89@gmail.com>
2022-09-27 00:55:11 +1000

    Shortened some config code.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,47 +1,46 @@
+local opt = vim.opt
+
 -- case insensitive searching
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
+opt.smartcase = true
+opt.ignorecase = true
 
 -- enable mouse interactivity
-vim.opt.mouse = 'a'
+opt.mouse = 'a'
 
 -- incremenetal search while typing
-vim.opt.incsearch = true
+opt.incsearch = true
 
 -- enable line numbers
-vim.opt.number = true
+opt.number = true
 
 -- highlight matching braces, brackets, etc
-vim.opt.showmatch = true
+opt.showmatch = true
 
 -- enable row/col ruler
-vim.opt.ruler = true
+opt.ruler = true
 
 -- allow backspace to delete these characters
-vim.opt.backspace = [[indent,eol,start]]
+opt.backspace = [[indent,eol,start]]
 
 -- hide buffers instead of closing them
-vim.opt.hidden = true
+opt.hidden = true
 
 -- don't wrap long lines
-vim.opt.wrap = false
+opt.wrap = false
 
 -- encoding used for display in the terminal/ui
-vim.opt.encoding = 'utf-8'
+opt.encoding = 'utf-8'
 
 -- encoding used when writing to file
-vim.opt.fileencoding = 'utf-8'
+opt.fileencoding = 'utf-8'
 
 -- large undo history
-vim.opt.history = 1000
-vim.opt.undolevels = 1000
+opt.history = 1000
+opt.undolevels = 1000
 
 -- auto set the working directory to the directory of the current file
-vim.opt.autochdir = true
+opt.autochdir = true
 
 -- whitespace characters
-vim.opt.listchars = 'tab:  |,lead:.,trail:.'
-vim.opt.list = true
-
--- enable high quality colours in terminal
-vim.opt.termguicolors = true
+opt.listchars = 'tab:  |,lead:.,trail:.'
+opt.list = true
--- lua/core/style.lua
+++ lua/core/style.lua
@@ -1,25 +1,25 @@
-vim.opt.background = 'dark'
+local opt = vim.opt
+
+opt.background = 'dark'
 
 if (vim.fn.has('termguicolors') == 1) then
-    vim.opt.termguicolors = true
+    opt.termguicolors = true
 end
 
-vim.cmd [[
-syntax enable
-]]
+vim.cmd('syntax enable')
 
 -- tab width equivalent in spaces
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+opt.tabstop = 4
+opt.shiftwidth = 4
 
 -- convert tabs to spaces
-vim.o.expandtab = true
+opt.expandtab = true
 
 -- retain indentation for new lines
-vim.o.autoindent = true
+opt.autoindent = true
 
 -- indent based on the code syntax
-vim.o.cindent = true
+opt.cindent = true
 
 -- disable automatic comment continuation
 vim.api.nvim_create_autocmd('FileType', {
