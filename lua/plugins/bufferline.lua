4bea93cbebf3a1ad99c87d9631471edb752d9007
Daniel Hack <hack.daniel.89@gmail.com>
2022-09-26 14:31:36 +1000

    Middle click closes buffer. Slanted tab styling.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,7 @@
 require('bufferline').setup({
     options = {
-        show_close_icon = false
+        show_close_icon = false,
+        separator_style = 'slant',
+        middle_mouse_command = 'bdelete! %d'
     }
 })
