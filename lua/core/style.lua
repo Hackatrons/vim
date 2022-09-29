95960db8e5740bd32896a5baf364d439a9b7fec5
Daniel Hack <hack.daniel.89@gmail.com>
2022-09-29 16:12:24 +1000

    Highlight on yank.

--- lua/core/style.lua
+++ lua/core/style.lua
@@ -22,11 +22,25 @@ opt.autoindent = true
 opt.cindent = true
 
 -- disable automatic comment continuation
+local cc_group = vim.api.nvim_create_augroup('CommentContinuation', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
     pattern = '*',
+    group = cc_group,
     command = 'setlocal formatoptions-=ro'
 })
 
 -- use devicons with neovide
 -- disable them for other UIs that haven't got a patched font configured
 vim.g.me_enable_icons = vim.g.neovide or false
+
+-- highlight selected text on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank({
+            timeout = 300,
+        })
+    end,
+    group = highlight_group,
+    pattern = '*'
+})
