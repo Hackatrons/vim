73a4f56ce466925b8bf9d2475787a53933bc3a9c
Daniel Hack <hack.daniel.89@gmail.com>
2022-10-03 19:02:52 +1100

    Telescope mappings to move up/down.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,17 @@
+local actions = require('telescope.actions')
+
 require('telescope').setup({
     defaults = {
         file_ignore_patterns = {
             '\\.git'
+        },
+        mappings = {
+            i = {
+                ['<a-j>'] = actions.move_selection_next,
+                ['<a-k>'] = actions.move_selection_previous,
+                ['<Tab>'] = actions.move_selection_next,
+                ['<S-Tab>'] = actions.move_selection_previous,
+            },
         }
     }
 })
