bfad37906cedae949c7d3359d5559b064adb8e49
Daniel Hack <hack.daniel.89@gmail.com>
2022-10-03 19:12:58 +1100

    Additional ignore patterns. Only show filename in prompt.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,14 +3,19 @@ local actions = require('telescope.actions')
 require('telescope').setup({
     defaults = {
         file_ignore_patterns = {
-            '\\.git'
+            '.git',
+            'node_modules',
+            'bin',
+            'obj'
         },
+        -- only show the filename, exclude path
+        path_display = { 'tail' },
         mappings = {
             i = {
                 ['<a-j>'] = actions.move_selection_next,
                 ['<a-k>'] = actions.move_selection_previous,
-                ['<Tab>'] = actions.move_selection_next,
-                ['<S-Tab>'] = actions.move_selection_previous,
+                ['<tab>'] = actions.move_selection_next,
+                ['<s-tab>'] = actions.move_selection_previous,
             },
         }
     }
