f221ac9107e8fc439157aa15b4f31a65ba0390ad
Daniel Hack <hack.daniel.89@gmail.com>
2022-10-04 14:32:46 +1100

    Disabled markdown width of 80 chars rule.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,7 +5,10 @@ require('mason-null-ls').setup({
 local null_ls = require('null-ls')
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.diagnostics.markdownlint.with {
+            -- disable max width 80 chars rule
+            extra_args = { '--disable', 'MD013' }
+        },
         null_ls.builtins.formatting.prettierd
     },
 })
