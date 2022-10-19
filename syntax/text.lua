fd7ee93d5af79c393486e8ea0086b334a325ce5c
Daniel Hack <hack.daniel.89@gmail.com>
2022-10-19 13:18:42 +1100

    Disabled spell checking for urls in text files.

---
+++ syntax/text.lua
@@ -0,0 +1,8 @@
+-- don't spellcheck urls
+-- regex description
+-- 1. match one or more word characters
+-- 2. until a ':'
+-- 3. then match all remaining non-whitespace characters
+vim.cmd([[
+    syn match UrlNoSpell '\w\+:\/\/\S\+' contains=@NoSpell
+]])
