bc9dfa265d16668dd64ed422d61acaec039f94c9
Daniel Hack <hack.daniel.89@gmail.com>
2023-02-22 05:37:40 +1100

    Used zig compiler for treesitter on Windows.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,8 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/1985
+if vim.fn.has('win32') == 1 then
+    require 'nvim-treesitter.install'.compilers = { 'zig' }
+end
+
 require('nvim-treesitter.configs').setup({
     ensure_installed = { 'lua', 'javascript', 'typescript', 'html', 'c_sharp' },
     highlight = {
