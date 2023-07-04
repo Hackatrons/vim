54d6bc16160380be0c4671da8db538964076f778
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-04 14:07:38 +1000

    Enabled file tree indent markers.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,11 +1,14 @@
 require("nvim-tree").setup({
-    sync_root_with_cwd =  true,
+    sync_root_with_cwd = true,
     actions = {
         open_file = {
             quit_on_open = true
         }
     },
     renderer = {
+        indent_markers = {
+            enable = true,
+        },
         icons = {
             show = {
                 file = vim.g.me_enable_icons,
