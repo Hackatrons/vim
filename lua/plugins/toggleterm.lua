5cbdca9ac16606664b244a700023978a00b98af1
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-09 21:17:00 +1000

    Detect OS before setting the shell.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,13 @@ return {
         require("toggleterm").setup({
             open_mapping = "<c-cr>",
             direction = "float",
-            shell = "pwsh.exe",
+            shell = function()
+                if vim.loop.os_uname().sysname == "Windows NT" and vim.fn.executable("pwsh.exe") then
+                    return "pwsh.exe"
+                end
+
+                return vim.o.shell
+            end,
             float_opts = {
                 border = "curved",
             }
