a8850150c9d0513c859a36eef97727904d267011
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-09 16:49:32 +1000

    Disabled treeview sync root with cwd.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -14,7 +14,6 @@ return {
     },
     config = function()
         require("nvim-tree").setup({
-            sync_root_with_cwd = true,
             actions = {
                 open_file = {
                     quit_on_open = true
