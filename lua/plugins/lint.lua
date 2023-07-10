2a004bf44a6b74e7166751e5b1ef6ee0828b1f1c
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-11 01:19:13 +1000

    Added gitsigns source to null-ls.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -23,6 +23,7 @@ return {
                 }),
                 null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.stylua,
+                null_ls.builtins.code_actions.gitsigns,
             },
         })
     end,
