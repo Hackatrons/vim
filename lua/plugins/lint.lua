db9047a6bfa88ac78749a24fb9d12b8df9bffae0
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-11 17:11:03 +1000

    Removed gitsigns from code actions as it's too noisy.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -26,10 +26,8 @@ return {
                     }),
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.code_actions.gitsigns,
                 },
             }
-            --
             null_ls.setup(opts)
         end,
     },
