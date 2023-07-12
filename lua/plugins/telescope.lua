24c9a6de615c16bcc497145e8674d1ff0e28c6f5
Daniel Hack <hack.daniel.89@gmail.com>
2023-07-12 15:49:40 +1000

    Fixed git ignore pattern.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
         return {
             defaults = {
                 file_ignore_patterns = {
-                    ".git",
+                    "\\.git",
                     "node_modules",
                     "bin",
                     "obj",
