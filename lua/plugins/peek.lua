a1280ceca1e46ba0b0588f4cafa3fe0d577f0351
Hackatrons <hack.daniel.89@gmail.com>
2025-06-26 10:13:57 +1000

    Changed markdown commands.

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -6,7 +6,7 @@ return {
         require("peek").setup({
             app = "browser",
         })
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        vim.api.nvim_create_user_command("MarkdownRender", require("peek").open, {})
+        vim.api.nvim_create_user_command("MarkdownClose", require("peek").close, {})
     end,
 }
